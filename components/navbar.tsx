24f2c241d0a952fff8267da442b71d72786abe72
xee95 <zeeshannawaz2017@gmail.com>
2023-10-13 19:28:06 +0500

    error removed

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = async () => {
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">Xee's STORE</p>
+            <p className="font-bold text-xl">Xee&apos;s STORE</p>
           </Link>
           
           <MainNavbar data={categories} />
